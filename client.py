b975605693398514bbdf196c137d0295a5d7145c
Samuli Peltomaa <samulipeltomaa@hotmail.com>
2021-03-10 23:32:46 +0200

    some fixes

--- client.py
+++ client.py
@@ -28,40 +28,43 @@ def run_game_logic(sock):
     board = []
     initialize_board(board)
     render_board(board)
-    try:
-        recv_data = sock.recv(256)
-        data = recv_data.decode().split(",")
-        if data[0] == "Role":
-            role = data[1]
-        elif data[0] == "Stats":
-            if data[1] == "W":
-                print("You Won :) Your stats: Wins: {}, Losses: {}")
-            else:
-                print("You Lost :( Your stats: Wins: {}, Losses: {}")
+    while True:
+        try:
+            recv_data = sock.recv(256)
+            data = recv_data.decode().split(",")
+            if data[0] == "Role":
+                role = data[1]
+            elif data[0] == "Stats":
+                if data[1] == "W":
+                    print("You Won :) Your stats: Wins: {}, Losses: {}")
+                else:
+                    print("You Lost :( Your stats: Wins: {}, Losses: {}")
+                input("Press Enter to continue...")
+                return
+            elif data[0] == "Move":
+                if role == 'X':
+                    set_board_value(board, int(data[1]), int(data[2]), 'O')
+                else:
+                    set_board_value(board, int(data[1]), int(data[2]), 'X')
+                render_board(board)
+            elif data[0] == "Turn":
+                try:
+                    x = int(input("Move to X: "))
+                    y = int(input("Move to Y: "))
+                    if x >= 3 or y >= 3 or x < 0 or y < 0 or  get_board_value(board, x, y) != " ":
+                        raise ValueError("")
+                except ValueError:
+                    print("Invalid Position or entered value!")
+
+                if x < 3 and y < 3 and x >= 0 and y >= 0 and get_board_value(board, x, y) == " ":
+                    set_board_value(board, x, y, role)
+                    sock.sendall("Move,{},{}".format(x, y).encode())
+                else:
+                    print("Invalid Board Position!!")
+        except:
+            print("Error occurred!")
             input("Press Enter to continue...")
             return
-        elif data[0] == "Move":
-            if role == 'X':
-                set_board_value(board, int(data[1]), int(data[2]), 'O')
-            else:
-                set_board_value(board, int(data[1]), int(data[2]), 'X')
-            render_board(board)
-        elif data[0] == "Turn":
-            try:
-                x = int(input("Move to X: "))
-                y = int(input("Move to Y: "))
-                if x >= 3 or y >= 3 or x < 0 or y < 0 or  get_board_value(board, x, y) != " ":
-                    raise ValueError("")
-            except ValueError:
-                print("Invalid Position or entered value!")
-
-            if x < 3 and y < 3 and x >= 0 and y >= 0 and get_board_value(board, x, y) == " ":
-                set_board_value(board, x, y, role)
-                sock.sendall("Move,{},{}".format(x, y).encode())
-            else:
-                print("Invalid Board Position!!")
-    except:
-        pass
 
 def run_tick_tack_tocker():
     server_address = ('localhost', 1999)
@@ -73,7 +76,8 @@ def run_tick_tack_tocker():
         try:
             sock.connect(server_address)
             sock.sendall("Login,{},{}".format(username, password).encode())
-            sock.recv(256)
+            data = sock.recv(256)
+            print(data.decode())
             run_game_logic(sock)
         except: 
             print("Failed to connect to server.")
--- game_matching.py
+++ game_matching.py
@@ -1,13 +1,11 @@
-#!/usr/bin/env python3
-
 import socket
 import sys
+import random
 
 GAME_MATCHING_PORT = 1999
 available_ports = [i for i in range(2000, 3000)]
 
 def start_game_matching():
-    print("toimii")
     available_players = []
     
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -26,17 +24,40 @@ def start_game_matching():
                 print("GAME_MATCHING: User exists.")
             else:
                 print("GAME_MATCHING: User does not exist. Creating new user...")
-                file.write(",".join(credentials))
+                file.write(",".join(credentials) + "\n")
                     
-        connection.encode().sendall("GAME_MATCHING: You are now being matched against another player.")
+        connection.sendall("GAME_MATCHING: You are now being matched against another player.".encode())
         available_players.append(connection)
         if len(available_players) >= 2:
+            print("GAME_MATCHING: 2 players found")
             create_game_instance("localhost", available_players[0], available_players[1], available_ports.pop(0))
-            del available_players[:2]
+            #del available_players[:2]
+            print("GAME_MATCHING: 2 players deleted")
             
 def create_game_instance(address, player_1, player_2, port):
-    player_1.sendall("GAME_INSTANCE: Player 2 connected.".encode())
-    player_2.sendall("GAME_INSTANCE: Player 1 connected.".encode())
+    print("GAME_INSTANCE: Called game instance")
+    result = player_1.sendall("GAME_INSTANCE: Player 2 connected.".encode())
+    print(result)
+    result_2 = player_2.sendall("GAME_INSTANCE: Player 1 connected.".encode())
+    print(result_2)
+    
+    x = random.randint(1,2)
+    if x == 1:
+        player_1.sendall("GAME_INSTANCE: Player 1 is X and will start.".encode())
+        player_2.sendall("GAME_INSTANCE: Player 2 is O and will go second.")
+        #while True:
+            #player_1.sendall("GAME_INSTANCE: Your turn. Make a move.")
+            #move = player_1.recv(64)
+            #check_move(move)
+        
+    else:
+        player_1.sendall("GAME_INSTANCE: Player 2 is X and will start.".encode())
+        player_2.sendall("GAME_INSTANCE: Player 1 is O and will go second.")
+        #while True:
+            #player_2.sendall("GAME_INSTANCE: Your turn. Make a move.")
+            #move = player_1.recv(64)
+            #check_move(move)
+            
 if __name__ == "__main__":
     start_game_matching()
     
