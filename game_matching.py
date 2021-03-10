41422bf127e86df8b621441227634bdb90a90b5c
Samuli Peltomaa <samulipeltomaa@hotmail.com>
2021-03-10 23:03:43 +0200

    server fixes

--- game_matching.py
+++ game_matching.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import socket
 import sys
 
@@ -5,18 +7,19 @@ GAME_MATCHING_PORT = 1999
 available_ports = [i for i in range(2000, 3000)]
 
 def start_game_matching():
+    print("toimii")
     available_players = []
     
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_address = ("localhost", 1999)
+    server_address = ("localhost", GAME_MATCHING_PORT)
     sock.bind(server_address)
     sock.listen(1)
     
     while True:
         print("GAME_MATCHING: Listening for connections...")
         connection, client_address = sock.accept()
-        data = connection.recv(256)
-        credentials = data.split(,)[1:]
+        data = connection.recv(256).decode()
+        credentials = data.split(",")[1:]
         with open("credentials.txt", "a+") as file:
             lines = [line.rstrip() for line in file]
             if ",".join(credentials) in lines:
@@ -25,18 +28,15 @@ def start_game_matching():
                 print("GAME_MATCHING: User does not exist. Creating new user...")
                 file.write(",".join(credentials))
                     
-        connection.sendall("GAME_MATCHING: You are now being matched against another player."
+        connection.encode().sendall("GAME_MATCHING: You are now being matched against another player.")
         available_players.append(connection)
         if len(available_players) >= 2:
             create_game_instance("localhost", available_players[0], available_players[1], available_ports.pop(0))
             del available_players[:2]
             
 def create_game_instance(address, player_1, player_2, port):
-    player_1.sendall("GAME_INSTANCE: Player 2 connected.") 
-    player_2.sendall("GAME_INSTANCE: Player 1 connected.") 
-    
-
-
-
-if __name__ == "main":
+    player_1.sendall("GAME_INSTANCE: Player 2 connected.".encode())
+    player_2.sendall("GAME_INSTANCE: Player 1 connected.".encode())
+if __name__ == "__main__":
     start_game_matching()
+    
